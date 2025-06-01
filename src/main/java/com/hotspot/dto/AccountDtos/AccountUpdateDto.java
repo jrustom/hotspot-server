c32f0c63f9e05d6c6c44520fbaa868ea1c207769
Jason Rustom <jasonrustom04@gmail.com>
2025-06-01 16:20:55 -0400

    added validation

--- src/main/java/com/hotspot/dto/AccountDtos/AccountUpdateDto.java
+++ src/main/java/com/hotspot/dto/AccountDtos/AccountUpdateDto.java
@@ -1,10 +1,12 @@
 package com.hotspot.dto.AccountDtos;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class AccountUpdateDto {
+    @NotBlank(message = "The new name must not be blank")
     private final String name;
 }
