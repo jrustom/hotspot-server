5b55190c7ee77185a4b433a8ea3e0f916b85fcc7
Jason Rustom <jasonrustom04@gmail.com>
2025-06-03 15:30:30 -0400

    added exception handling for jakarta validation exceptions

--- src/main/java/com/hotspot/exceptions/HotspotExceptionHandler.java
+++ src/main/java/com/hotspot/exceptions/HotspotExceptionHandler.java
@@ -1,6 +1,12 @@
 package com.hotspot.exceptions;
 
+import java.util.ArrayList;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -12,5 +18,16 @@ public class HotspotExceptionHandler {
     @ExceptionHandler(HotspotException.class)
     public ResponseEntity<ErrorDto> handleHotspotException(HotspotException e) {
         return new ResponseEntity<>(new ErrorDto(e.getMessage(), e.getErrorCode()), e.getStatus());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ArrayList<ErrorDto>> handleJakartaValidationException(MethodArgumentNotValidException e) {
+        ArrayList<ErrorDto> errorMessages = new ArrayList<>();
+
+        for (FieldError error : e.getBindingResult().getFieldErrors()) {
+            errorMessages.add(new ErrorDto(error.getDefaultMessage(), 100));
+        }
+
+        return new ResponseEntity<>(errorMessages, HttpStatus.BAD_REQUEST);
     }
 }
