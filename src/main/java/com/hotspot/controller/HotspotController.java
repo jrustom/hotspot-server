bd8313c3a184449f07ff05dbd94eeb8446f4fec6
jrustom <jasonrustom04@gmail.com>
2025-08-11 23:15:18 -0400

    test

--- src/main/java/com/hotspot/controller/HotspotController.java
+++ src/main/java/com/hotspot/controller/HotspotController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hotspot.dto.HotspotDtos.HotspotRequestDto;
-import com.hotspot.dto.HotspotDtos.HotspotResponseDto;
+import com.hotspot.dto.HotspotDtos.HotdpotResponseDto;
 import com.hotspot.model.User.VoteType;
 import com.hotspot.services.HotspotService;
 
