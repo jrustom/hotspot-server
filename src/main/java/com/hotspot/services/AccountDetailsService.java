38cd7607db5264ca24088c5b74e98c903ed32c63
jrustom <jasonrustom04@gmail.com>
2025-10-14 00:46:27 -0400

    added account details service which will fetch the user by their username and return a UserDetails instance with the correct values

---
+++ src/main/java/com/hotspot/services/AccountDetailsService.java
@@ -0,0 +1,24 @@
+package com.hotspot.services;
+
+import com.hotspot.model.User;
+import com.hotspot.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AccountDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User not found."));
+
+        return org.springframework.security.core.userdetails.User.withUsername(user.getUsername()).password(user.getPassword()).authorities("USER").build();
+    }
+
+}
