f4251a7c4841cc179d89d8d45ac51aa4448b8b37
Jason Rustom <jasonrustom04@gmail.com>
2025-10-14 18:47:56 -0400

    using context id for chats/messages

--- src/main/java/com/hotspot/controller/ChatController.java
+++ src/main/java/com/hotspot/controller/ChatController.java
@@ -1,29 +1,20 @@
 package com.hotspot.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.hotspot.dto.MessageDtos.MessageResponseDto;
 import com.hotspot.services.ChatService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/chats/")
+@RequiredArgsConstructor
 public class ChatController {
-    private ChatService chatService;
-
-    @Autowired
-    public ChatController(ChatService chatService) {
-        this.chatService = chatService;
-    }
+    private final ChatService chatService;
 
     // Get messages for a chat
     @GetMapping("{cid}")
     public MessageResponseDto[] getMessages(@PathVariable(name = "cid") String chatId,
-            @RequestParam(name = "count", required = false) Integer messageCount) {
+                                            @RequestParam(name = "count", required = false) Integer messageCount) {
         return chatService.getSpecificMessages(chatId, messageCount);
     }
 
